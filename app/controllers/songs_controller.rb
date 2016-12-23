bfd733e5fb2f716d2a4dbf48d302f12baecf8456
mcclains2003 <mcclains2003@gmail.com>
2016-12-23 13:54:59 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,13 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    preference = Preference.first
+
+    if preference.allow_create_songs == false
+      redirect_to songs_path
+    else
+      @song = Song.new
+    end
   end
 
   def create
